10e544bf18e43bb85017cd7457c7bdb99ed221c7
Chris Foo <foochris@gmail.com>
2016-05-04 11:39:12 -0400

    missing commit
    [#109971048]

--- app/services/picture.service.js
+++ app/services/picture.service.js
@@ -5,7 +5,7 @@
         .module('app.core')
         .service('pictureService', pictureService);
 
-    function pictureService($cordovaCamera, $cordovaFile, $cordovaFileTransfer, $ionicHistory, $ionicPopup, $q, $state, $stateParams, $translate, apiUrlService, localStorageService, lodash, md5, tripService){
+    function pictureService($cordovaFile, $cordovaFileTransfer, $ionicHistory, $ionicPopup, $q, $state, $stateParams, $translate, apiUrlService, localStorageService, lodash, md5, tripService){
         var _ = lodash;
         var pictureData = null;
         var service = {
@@ -13,21 +13,15 @@
             upload: upload
         };
 
-        var pictureOptions = {
-                targetWidth: 1024,
-                targetHeight: 1024,
-                quality: 50
-            };
-
         return service;
 
         function getPicture(type, data) {
             pictureData = data;
-            // var pictureOptions = {
-            //     targetWidth: 1024,
-            //     targetHeight: 1024,
-            //     quality: 50
-            // };
+            var pictureOptions = {
+                targetWidth: 1024,
+                targetHeight: 1024,
+                quality: 50
+            };
 
             if (type == 'camera') {
                 _.assignIn(
@@ -57,55 +51,26 @@
         }
 
         function saveSelectedPictureLocalStorage(fileURI) {
+            var picturesLocalStorage = tripService.getDraft($stateParams.tripId, 'pictures');
+
+            if (_.isEmpty(picturesLocalStorage)) {
+                picturesLocalStorage = [];
+            }
+
             getFileUriData(fileURI).then(function(file){
-                window.resolveLocalFileSystemURL(
-                    fileURI,
+                if (cordova.file.tempDirectory === null) {
+                    selectPictureSuccess(file, fileURI, fileURI, picturesLocalStorage);
 
-                    function(fileEntry) {
-                        var fileExt = '.' + fileURI.split('.').pop();
-                        var newFileName = md5.createHash(fileURI.toString()) + fileExt;
+                } else {
+                    var fileExt = '.' + file.name.split('.').pop();
+                    var newFileName = md5.createHash(file.name + Math.random()) + fileExt;
 
-                        window.resolveLocalFileSystemURL(cordova.file.dataDirectory,
-                                function(dirEntry) {
-                                    // move the file to a new directory and rename it
-                                    fileEntry.moveTo(dirEntry, newFileName, successCallback, selectPictureFail);
-                                },
-                                selectPictureFail);
-                    },
-                    selectPictureFail
-                );
-
-                function successCallback(entry) {
-                    var filepath = cordova.file.dataDirectory + entry.name;
-
-                    var fileUriData = {
-                        'id' : md5.createHash(fileURI.toString()),
-                        'filename' : file.name,
-                        'filepath' : filepath,
-                        'caption' : (pictureData.caption ? pictureData.caption : '' ),
-                        'filesize' : file.size
-                    };
-
-                    var picturesLocalStorage = tripService.getDraft($stateParams.tripId, 'pictures');
-
-                    if (_.isEmpty(picturesLocalStorage)) {
-                        picturesLocalStorage = [];
-                    }
-
-                    // remove selected picture from array if it exists
-                    picturesLocalStorage = _.filter(picturesLocalStorage, function(o) {
-                        return o.filename !== file.name;
-                    });
-
-                    var pictures = picturesLocalStorage.concat(fileUriData);
-                    tripService.setDraft($stateParams.tripId, 'pictures', pictures);
-
-                    $ionicPopup.alert({
-                        title: $translate.instant('controller.report.picture.upload.title'),
-                        template: $translate.instant('controller.report.picture.upload.selected.title')
-                    }).then(function(){
-                        $ionicHistory.goBack(-1);
-                    });
+                    $cordovaFile.copyFile(cordova.file.tempDirectory, file.name, cordova.file.dataDirectory, newFileName).then(
+                        function(success) {
+                            selectPictureSuccess(file, cordova.file.dataDirectory + newFileName, fileURI, picturesLocalStorage);
+                        },
+                        selectPictureFail
+                    );
                 }
             });
 
@@ -120,9 +85,28 @@
 
                 return deferred.promise;
             }
+
+            function selectPictureSuccess(file, filepath, fileURI, picturesLocalStorage) {
+                var fileUriData = {
+                            'id' : md5.createHash(fileURI.toString()),
+                            'filepath' : filepath,
+                            'caption' : (pictureData.caption ? pictureData.caption : '' ),
+                            'filesize' : file.size
+                        };
+
+                var pictures = picturesLocalStorage.concat(fileUriData);
+                tripService.setDraft($stateParams.tripId, 'pictures', pictures);
+
+                $ionicPopup.alert({
+                    title: $translate.instant('controller.report.picture.upload.title'),
+                    template: $translate.instant('controller.report.picture.upload.selected.title')
+                }).then(function(){
+                    $ionicHistory.goBack(-1);
+                });
+            }
         }
 
-        function selectPictureFail(message) {
+        function selectPictureFail(error) {
             $ionicPopup.alert({
                 title: $translate.instant('controller.report.picture.upload.title'),
                 template: $translate.instant('controller.report.picture.upload.selected.failed.template')
