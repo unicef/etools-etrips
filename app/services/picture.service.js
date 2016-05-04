9392044793b5583dcbe29afd3c0b0284eb2e80c6
Chris Foo <foochris@gmail.com>
2016-05-04 01:08:06 -0400

    store selected image in cordova file storage
    [#109971048]

--- app/services/picture.service.js
+++ app/services/picture.service.js
@@ -5,7 +5,7 @@
         .module('app.core')
         .service('pictureService', pictureService);
 
-    function pictureService($cordovaFileTransfer, $ionicHistory, $ionicPopup, $q, $state, $stateParams, $translate, apiUrlService, localStorageService, lodash, md5, tripService){
+    function pictureService($cordovaCamera, $cordovaFile, $cordovaFileTransfer, $ionicHistory, $ionicPopup, $q, $state, $stateParams, $translate, apiUrlService, localStorageService, lodash, md5, tripService){
         var _ = lodash;
         var pictureData = null;
         var service = {
@@ -13,15 +13,21 @@
             upload: upload
         };
 
-        return service;
-
-        function getPicture(type, data) {
-            pictureData = data;
-            var pictureOptions = {
+        var pictureOptions = {
                 targetWidth: 1024,
                 targetHeight: 1024,
                 quality: 50
             };
+
+        return service;
+
+        function getPicture(type, data) {
+            pictureData = data;
+            // var pictureOptions = {
+            //     targetWidth: 1024,
+            //     targetHeight: 1024,
+            //     quality: 50
+            // };
 
             if (type == 'camera') {
                 _.assignIn(
@@ -30,7 +36,7 @@
                         destinationType: navigator.camera.DestinationType.FILE_URI,
                         sourceType: navigator.camera.PictureSourceType.CAMERA,
                         saveToPhotoAlbum: true
-                    }                    
+                    }
                 );
 
             } else if (type == 'photolibrary') {
@@ -39,7 +45,7 @@
                     {
                         destinationType: navigator.camera.DestinationType.FILE_URI,
                         sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
-                    }                    
+                    }
                 );
             }
 
@@ -51,42 +57,61 @@
         }
 
         function saveSelectedPictureLocalStorage(fileURI) {
-            if (fileURI.substring(0,21)=="content://com.android") {
-                var photo_split = fileURI.split("%3A");
-                fileURI = "content://media/external/images/media/" + photo_split[1];
-            }
-
-            var picturesLocalStorage = tripService.getDraft($stateParams.tripId, 'pictures');
-
-            if (_.isEmpty(picturesLocalStorage)) {
-                picturesLocalStorage = [];
-            }
-
-            // remove selected picture from array if it exists
-            picturesLocalStorage = _.filter(picturesLocalStorage, function(o) { return o.filepath !== fileURI; });
-
             getFileUriData(fileURI).then(function(file){
-                var fileUriData = { 
-                    'id' : md5.createHash(fileURI.toString()), 
-                    'filepath' : fileURI, 
-                    'caption' : (pictureData.caption ? pictureData.caption : '' ), 
-                    'filesize' : file.size
-                };
+                window.resolveLocalFileSystemURL(
+                    fileURI,
 
-                var pictures = picturesLocalStorage.concat(fileUriData);
-                tripService.setDraft($stateParams.tripId, 'pictures', pictures);
+                    function(fileEntry) {
+                        var fileExt = '.' + fileURI.split('.').pop();
+                        var newFileName = md5.createHash(fileURI.toString()) + fileExt;
 
-                $ionicPopup.alert({
-                    title: $translate.instant('controller.report.picture.upload.title'),
-                    template: $translate.instant('controller.report.picture.upload.selected.title')
-                }).then(function(){
-                    $ionicHistory.goBack(-1);
-                });
+                        window.resolveLocalFileSystemURL(cordova.file.dataDirectory,
+                                function(dirEntry) {
+                                    // move the file to a new directory and rename it
+                                    fileEntry.moveTo(dirEntry, newFileName, successCallback, selectPictureFail);
+                                },
+                                selectPictureFail);
+                    },
+                    selectPictureFail
+                );
+
+                function successCallback(entry) {
+                    var filepath = cordova.file.dataDirectory + entry.name;
+
+                    var fileUriData = {
+                        'id' : md5.createHash(fileURI.toString()),
+                        'filename' : file.name,
+                        'filepath' : filepath,
+                        'caption' : (pictureData.caption ? pictureData.caption : '' ),
+                        'filesize' : file.size
+                    };
+
+                    var picturesLocalStorage = tripService.getDraft($stateParams.tripId, 'pictures');
+
+                    if (_.isEmpty(picturesLocalStorage)) {
+                        picturesLocalStorage = [];
+                    }
+
+                    // remove selected picture from array if it exists
+                    picturesLocalStorage = _.filter(picturesLocalStorage, function(o) {
+                        return o.filename !== file.name;
+                    });
+
+                    var pictures = picturesLocalStorage.concat(fileUriData);
+                    tripService.setDraft($stateParams.tripId, 'pictures', pictures);
+
+                    $ionicPopup.alert({
+                        title: $translate.instant('controller.report.picture.upload.title'),
+                        template: $translate.instant('controller.report.picture.upload.selected.title')
+                    }).then(function(){
+                        $ionicHistory.goBack(-1);
+                    });
+                }
             });
 
             function getFileUriData(fileURI) {
                 var deferred = $q.defer();
-                
+
                 window.resolveLocalFileSystemURL(fileURI, function(fileEntry){
                     fileEntry.file(function(file) {
                         deferred.resolve(file);
@@ -119,7 +144,7 @@
 
             var uploadUrl = apiUrlService.BASE() + '/trips/api/' + data.trip_id + '/upload/';
 
-            return $cordovaFileTransfer.upload(uploadUrl, data.filepath, options, true);     
+            return $cordovaFileTransfer.upload(uploadUrl, data.filepath, options, true);
         }
     }
 
