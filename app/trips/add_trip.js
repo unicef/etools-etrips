b3171611df1cf7411b3af812d5f06be520fa16cc
Chris Foo <foochris@gmail.com>
2016-06-09 22:55:29 -0400

    apply eTools rules to eTrips (owner/supervisor and to/from date)
    [#113390421]

--- app/trips/add_trip.js
+++ app/trips/add_trip.js
@@ -50,18 +50,26 @@
 
         function isFormValid() {
             var isFormValid = true;
-            vm.error = {};
 
-            // validate data
+            // validate purpose_of_travel
             if (!_.isUndefined(vm.trip.purpose_of_travel) && vm.trip.purpose_of_travel.length > 254) {
                 vm.trip.purpose_of_travel = vm.trip.purpose_of_travel.substring(0, 254);
                 isFormValid = false;
+
             }
 
             // validate required fields
+            var hasOneInvalidField = false;
+
             _.each(requiredFields, function(requiredField){
-                isFormValid = isFormFieldValid(requiredField);
+                if (isFormFieldValid(requiredField) === false) {
+                    hasOneInvalidField = true;
+                }
             });
+
+            if (hasOneInvalidField === true) {
+                isFormValid = false;
+            }
 
             return isFormValid;
         }
@@ -69,11 +77,52 @@
         function isFormFieldValid(field) {
             var isFormValid = true;
 
+            // empty field
             if (_.isUndefined(vm.trip[field]) || _.isNull(vm.trip[field]) || vm.trip[field].length === 0) {
                 vm.error[field] = true;
+                vm.error[field + '_message'] = 'template.trip.add_trip.error.cant_leave_empty';
                 isFormValid = false;
+
             } else {
                 vm.error[field] = false;
+                isFormValid = true;
+            }
+
+            // from_date must be less than to_date
+            if (!_.isUndefined(vm.trip.from_date) && !_.isUndefined(vm.trip.to_date)) {
+                var fromDateEpoch = moment(new Date(vm.trip.from_date)).valueOf();
+                var toDateEpoch = moment(new Date(vm.trip.to_date)).valueOf();
+
+                if (fromDateEpoch > toDateEpoch) {
+                    vm.error.from_date = true;
+                    vm.error.from_date_message = 'template.trip.add_trip.error.to_date_less_than_from';
+                    vm.error.to_date = true;
+                    vm.error.to_date_message = 'template.trip.add_trip.error.to_date_less_than_from';
+                    isFormValid = false;
+
+                } else {
+                    vm.error.from_date = false;
+                    vm.error.to_date = false;
+                }
+            }
+
+            // a supervisor can't be a traveller
+            if (!_.isUndefined(vm.trip.traveller) && !_.isUndefined(vm.trip.supervisor)){
+
+                if (vm.trip.traveller.length > 0 &&
+                    vm.trip.supervisor > -1 &&
+                    _.intersection(vm.trip.traveller, [vm.trip.supervisor.toString()]).length > 0) {
+
+                    vm.error.traveller = true;
+                    vm.error.traveller_message = 'template.trip.add_trip.error.owner_is_supervisor';
+                    vm.error.supervisor = true;
+                    vm.error.supervisor_message = 'template.trip.add_trip.error.owner_is_supervisor';
+                    isFormValid = false;
+
+                } else {
+                    vm.error.traveller = false;
+                    vm.error.supervisor = false;
+                }
             }
 
             return isFormValid;
@@ -83,34 +132,37 @@
             var isAddTripValid = isFormValid();
 
             if (isAddTripValid === true) {
+                // create new trip object;
+                var trip =  _.cloneDeep(vm.trip);
+
                 // format from_date and to_date from input date format to YYYY-MM-DD
                 var dateFields = ['from_date', 'to_date'];
 
                 _.each(dateFields, function(dateField){
                     var date = new Date(vm.trip[dateField]);
                     var formattedDate = moment(date).format('YYYY-MM-DD');
-                    vm.trip[dateField] = formattedDate;
+                    trip[dateField] = formattedDate;
                 });
 
                 // copy traveller value to owner field
-                vm.trip.owner = parseInt(vm.trip.traveller[0]);
+                trip.owner = parseInt(vm.trip.traveller[0]);
 
                 // set required fields to empty
                 var emptyFields = ['triplocation_set', 'travelroutes_set', 'tripfunds_set', 'actionpoint_set'];
 
                 _.each(emptyFields, function(emptyField) {
-                    vm.trip[emptyField] = [];
+                    trip[emptyField] = [];
                 });
 
                 // default values
-                vm.trip.created_date = moment();
-                vm.trip.travel_type = 'technical_support';
+                trip.created_date = moment();
+                trip.travel_type = 'technical_support';
 
                 $ionicLoading.show({
                     template: '<loading message="saving_trip"></loading>'
                 });
 
-                tripService.saveTrip(vm.trip).then(
+                tripService.saveTrip(trip).then(
                     function() {
                         $ionicLoading.hide();
 
